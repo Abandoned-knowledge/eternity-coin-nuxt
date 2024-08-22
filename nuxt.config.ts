fe8bfea05f95451ebb8ffe5ba7a41cca65634597
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-22 22:45:11 +0300

    install tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  srcDir: 'src/',
+  modules: ["@nuxtjs/tailwindcss"],
+  srcDir: "src/",
+
   dir: {
-    layouts: 'app/layouts',
+    layouts: "app/layouts",
   },
+
   imports: {
-    dirs: [
-      'shared/composables',
-    ],
+    dirs: ["shared/composables"],
   },
-})
+});
