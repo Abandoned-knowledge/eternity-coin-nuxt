14fc11b860e9586d4a7f8b9ec4a50d4b54aa3a55
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-22 22:57:53 +0300

    install dayjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/google-fonts"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/google-fonts", "dayjs-nuxt"],
   srcDir: "src/",
 
   dir: {
