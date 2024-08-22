811bb70e45289db2d9bfe711660a4892b20859a0
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-22 22:59:37 +0300

    install primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/google-fonts", "dayjs-nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@nuxtjs/google-fonts",
+    "dayjs-nuxt",
+    "nuxt-primevue"
+  ],
   srcDir: "src/",
 
   dir: {
