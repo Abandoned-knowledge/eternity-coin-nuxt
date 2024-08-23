44053cacf59ecc6c9afa96c30028aabbb63b2cf3
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-23 19:45:37 +0300

    insert donutChart to index page

--- src/pages/index/ui/index.vue
+++ src/pages/index/ui/index.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <FrameLayout title="frame"> index page </FrameLayout>
+  <div class="mt-10 flex items-center justify-center">
+    <FrameLayout title="frame" class="w-[300px]">
+      <ChartDonut />
+    </FrameLayout>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
