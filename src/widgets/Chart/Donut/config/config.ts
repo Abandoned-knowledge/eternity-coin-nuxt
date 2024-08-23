37061174952a107150e4a19824714c4c98ff37e7
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-23 20:08:29 +0300

    add cursor pointer on hover

--- src/widgets/Chart/Donut/config/config.ts
+++ src/widgets/Chart/Donut/config/config.ts
@@ -3,6 +3,12 @@ import textCenterOnHover from "./plugins/textCenterOnHover";
 export const plugins = [textCenterOnHover];
 
 export const options: ChartOptions = {
+  interaction: { mode: "index" },
+  onHover(event, elements) {
+    const chartElement = event.native?.target as HTMLElement;
+    const activeElement = elements[0];
+    chartElement.style.cursor = activeElement ? "pointer" : "auto";
+  },
   responsive: true,
   plugins: {
     legend: {
