b40283c22665c006ddd1c4b123ccb3ff66fb75bc
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-23 19:45:14 +0300

    create DonutChart -> ui

---
+++ src/widgets/Chart/Donut/index.ts
@@ -0,0 +1,6 @@
+import DonutChart from "./ui/DonutChart.vue";
+import { options, plugins } from "./config/config";
+import data from "./api/data";
+
+export { options, plugins, data };
+export default DonutChart;
---
+++ src/widgets/Chart/Donut/ui/DonutChart.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { options, plugins, data } from "../";
+
+import type { ChartData } from "chart.js";
+
+const chartData = computed(() => {
+  return {
+    labels: data.map((el) => el.label),
+    datasets: [
+      {
+        label: "Sales",
+        data: data.map((el) => el.data),
+        backgroundColor: data.map((el) => el.color),
+      },
+    ],
+  } as ChartData;
+});
+</script>
+
+<template>
+  <Chart type="doughnut" :data="chartData" :options="options" :plugins="plugins" />
+</template>
+
+<style lang="scss" scoped></style>
