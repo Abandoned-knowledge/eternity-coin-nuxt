66ec1bdd79eaa037f20ca6cb4523be8c66bc3e38
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-23 19:43:52 +0300

    create DonutChart -> fakeData

---
+++ src/widgets/Chart/Donut/api/data.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: "Продажи",
+    data: 15,
+    color: "#007bff",
+  },
+  {
+    label: "Прибыль",
+    data: 12,
+    color: "#28a745",
+  },
+  {
+    label: "Расходы",
+    data: 8,
+    color: "#dc3545",
+  },
+  {
+    label: "Посещаемость",
+    data: 4,
+    color: "#ffc107",
+  },
+];
