39c48232c9925303ef8ad847d3ecf2a5ae7ba02e
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-24 22:20:17 +0300

    add PopupAddTransaction to pages/index

--- src/pages/index/ui/index.vue
+++ src/pages/index/ui/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mt-10 flex h-max items-center">
-    <ChartDonut type="income" />
-    <ChartDonut type="expense" />
+  <div class="mt-10 flex h-max items-center gap-10">
+    <ChartDonut type="income" class="w-1/3"/>
+    <PopupAddTransaction />
+    <ChartDonut type="expense" class="w-1/3" />
   </div>
 </template>
