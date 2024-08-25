6298eafb00ccd16a63c24579851ba015e70283f8
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-25 12:08:56 +0300

    import components to pages/categories

--- src/pages/categories/ui/index.vue
+++ src/pages/categories/ui/index.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AllCategories from "./AllCategories.vue";
+import CreateCategory from "./CreateCategory.vue";
+</script>
 
 <template>
-  categories
-</template>
+  <div class="mt-5 flex flex-col gap-5 md:flex-row">
+    <AllCategories type="income" class="md:w-1/2" />
+    <AllCategories type="expense" class="md:w-1/2" />
+  </div>
 
-<style lang="scss" scoped></style>
+  <CreateCategory class="w-full mt-5" />
+</template>
