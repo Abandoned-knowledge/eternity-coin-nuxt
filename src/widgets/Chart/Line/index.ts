58668ca9460317a46138d4b760186e6542b9fd1a
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-26 13:20:44 +0300

    create widgets -> LineChart

---
+++ src/widgets/Chart/Line/index.ts
@@ -0,0 +1,2 @@
+import LineChart from "./ui/LineChart.vue";
+export default LineChart;
---
+++ src/widgets/Chart/Line/ui/LineChart.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Canvas from "@/features/Chart/Line";
+const props = defineProps<{ type: transactionType }>();
+</script>
+
+<template>
+  <FrameLayout :title="props.type">
+    <Canvas :type="props.type"/>
+  </FrameLayout>
+</template>
