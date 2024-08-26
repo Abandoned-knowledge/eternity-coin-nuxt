06f92390caf7cda6b17a434821e1179b422c91f0
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-26 21:34:09 +0300

    add custom style to pages/statistics -> tabs

--- src/pages/statistics/ui/index.vue
+++ src/pages/statistics/ui/index.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
-import Table from './Table.vue';
+import Table from "./Table.vue";
 
 const tabs: transactionType[] = ["expense", "income"];
 const innerTabs = ["charts", "table"];
 </script>
 
 <template>
-  <Tabs value="expense">
-    <TabList>
-      <Tab pt:root="w-1/2" v-for="tab in tabs" :value="tab">{{ tab }}</Tab>
+  <Tabs unstyled value="expense">
+    <TabList pt:activeBar="hidden" pt:tabList="flex gap-2 justify-center mt-5" class="main-tabs">
+      <Tab v-for="tab in tabs" :value="tab">{{ tab }}</Tab>
     </TabList>
     <TabPanels>
       <TabPanel v-for="tab in tabs" :value="tab">
-        <Tabs value="charts">
-          <TabLIst>
-            <Tab pt:root="w-1/2" v-for="innerTab in innerTabs" :value="innerTab">
+        <Tabs unstyled value="charts">
+          <TabList
+            pt:activeBar="hidden"
+            pt:tabList="flex gap-2 justify-center mt-3"
+            class="inner-tabs"
+          >
+            <Tab v-for="innerTab in innerTabs" :value="innerTab">
               {{ innerTab }}
             </Tab>
-          </TabLIst>
-          <TabPanels>
+          </TabList>
+          <TabPanels pt:root="mt-2">
             <TabPanel value="charts" class="flex flex-col gap-5 md:flex-row">
               <ChartDonut :type="tab" class="w-full md:w-1/3" />
               <ChartLine :type="tab" class="w-full md:flex-grow" />
             </TabPanel>
-            <TabPanel value="table"> 
-              <Table :type="tab"/>  
+            <TabPanel value="table">
+              <Table :type="tab" />
             </TabPanel>
           </TabPanels>
         </Tabs>
@@ -34,7 +38,21 @@ const innerTabs = ["charts", "table"];
 </template>
 
 <style lang="scss" scoped>
-.p-tabpanels {
-  @apply px-0;
+.main-tabs {
+  .p-tab {
+    @apply rounded border border-primary px-5 py-2 text-lg;
+    &-active {
+      @apply bg-primary font-medium text-white;
+    }
+  }
+}
+
+.inner-tabs {
+  .p-tab {
+    @apply rounded border border-primary px-3 py-1;
+    &-active {
+      @apply bg-primary font-medium text-white;
+    }
+  }
 }
 </style>
