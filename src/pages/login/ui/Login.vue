e5f1cbc2b3cc029a059b1b9c9eb429e021428944
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-31 11:12:30 +0300

    create pages -> login

---
+++ src/pages/login/ui/Login.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const inputLogin = ref();
+const inputPassword = ref();
+onMounted(() => {
+  setPageLayout("clear");
+});
+</script>
+
+<template>
+  <FrameLayout title="Login" class="w-1/2">
+    <form @submit.prevent class="flex flex-col items-center gap-10 py-5">
+      <FloatLabel class="w-full">
+        <InputText type="email" class="w-full" id="login" v-model="inputLogin" />
+        <label for="login">Login</label>
+      </FloatLabel>
+
+      <FloatLabel class="w-full">
+        <Password
+          pt:root="w-full"
+          pt:pcInput:root="w-full"
+          inputId="password"
+          v-model="inputPassword"
+          toggleMask
+          :feedback="false"
+        />
+        <label for="password">Password</label>
+      </FloatLabel>
+
+      <Button label="Login" type="submit" severity="contrast" />
+    </form>
+  </FrameLayout>
+</template>
