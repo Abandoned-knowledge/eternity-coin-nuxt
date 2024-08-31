061b397d1f2da9fe857d050f63f2973a4b8eb54c
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-31 20:58:15 +0300

    add validation to LoginForm

--- src/pages/login/ui/Login.vue
+++ src/pages/login/ui/Login.vue
@@ -1,30 +1,52 @@
 <script setup lang="ts">
-const inputLogin = ref();
-const inputPassword = ref();
-onMounted(() => {
-  setPageLayout("clear");
+import { object, string } from "yup";
+
+const schema = object({
+  login: string().required().email().label("Email address"),
+  password: string().required().label("Password"),
 });
+
+const { defineField, errors } = useForm({
+  validationSchema: schema,
+});
+
+const [login] = defineField("login");
+const [password] = defineField("password");
+
+setPageLayout("clear");
 </script>
 
 <template>
   <FrameLayout title="Login" class="w-full md:w-1/2">
     <form @submit.prevent class="form">
-      <FloatLabel class="w-full">
-        <InputText type="email" class="w-full" id="login" v-model="inputLogin" />
-        <label for="login">Login</label>
-      </FloatLabel>
+      <div class="w-full">
+        <FloatLabel>
+          <InputText
+            type="email"
+            id="login"
+            v-model="login"
+            :class="{ 'p-invalid': errors.login }"
+          />
+          <label for="login">Login</label>
+        </FloatLabel>
 
-      <FloatLabel class="w-full">
-        <Password
-          pt:root="w-full"
-          pt:pcInput:root="w-full"
-          inputId="password"
-          v-model="inputPassword"
-          toggleMask
-          :feedback="false"
-        />
-        <label for="password">Password</label>
-      </FloatLabel>
+        <small class="ml-1 text-warning">{{ errors.login }}</small>
+      </div>
+
+      <div class="w-full">
+        <FloatLabel class="w-full">
+          <Password
+            pt:root="w-full"
+            inputId="password"
+            v-model="password"
+            toggleMask
+            :feedback="false"
+          />
+          <label for="password">Password</label>
+        </FloatLabel>
+
+        <small class="ml-1 text-warning">{{ errors.password }}</small>
+      </div>
 
       <Button label="Login" type="submit" severity="contrast" />
     </form>
