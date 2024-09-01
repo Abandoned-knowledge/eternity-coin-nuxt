640a253f6f54994bdf37b4c85963c2945ca128c6
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-09-01 21:29:44 +0300

    create app.vue

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,14 +1,5 @@
 <script setup lang="ts">
-const windowStore = useWindowStore();
-
 import UpdateCategory from "~/features/Popup/UpdateCategory";
-onMounted(() => {
-  windowStore.isMobile = window.innerWidth <= 768;
-  window.addEventListener("resize", function () {
-    const width = this.innerWidth;
-    windowStore.isMobile = width <= 768;
-  });
-});
 </script>
 
 <template>
