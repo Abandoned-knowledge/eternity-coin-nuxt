9788cdd1203c261dfc7e40e8e6166f97cfd6e010
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-09-02 18:25:44 +0300

    fix: add loading animation to header -> profile button

--- src/widgets/header/ui/Buttons.vue
+++ src/widgets/header/ui/Buttons.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 const userStore = useUserStore();
+const loading = computed(() => !userStore.user);
 
-const label = computed(() => (userStore.user ? userStore.user.name : "none"));
+const label = computed(() => (userStore.user ? userStore.user.name : "waiting"));
 </script>
 
 <template>
   <div class="flex gap-3">
-    <Button :label="label" severity="contrast" as="router-link" to="/profile" />
+    <Button :loading="loading" :label="label" severity="contrast" as="router-link" to="/profile" />
     <Button @click="userStore.logOut" class="aspect-square" severity="danger" outlined>
       <LogOutIcon class="h-full w-full" />
     </Button>
