73600dff3fff89d9bd9a97b5b0b5d91caab307a9
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-09-02 18:11:49 +0300

    create logOut in store -> user

--- src/shared/store/user.ts
+++ src/shared/store/user.ts
@@ -2,10 +2,15 @@ export const useUserStore = defineStore("user", () => {
   const user = ref<IUser | null>(null);
 
   function getUser() {
-    const userLocalStorage = localStorage.getItem('user');
-    if (userLocalStorage) {
-        user.value = JSON.parse(userLocalStorage);
-    }
+    const userLocalStorage = localStorage.getItem("user");
+    if (userLocalStorage) user.value = JSON.parse(userLocalStorage);
   }
-  return { user, getUser };
+
+  function logOut() {
+    user.value = null;
+    localStorage.removeItem("user");
+    navigateTo("/login");
+  }
+
+  return { user, getUser, logOut };
 });
--- src/widgets/header/ui/Buttons.vue
+++ src/widgets/header/ui/Buttons.vue
@@ -1,10 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const userStore = useUserStore();
+
+const label = computed(() => (userStore.user ? userStore.user.name : "none"));
+</script>
 
 <template>
   <div class="flex gap-3">
-    <Button label="username" severity="contrast" as="router-link" to="/profile" />
+    <Button :label="label" severity="contrast" as="router-link" to="/profile" />
     <Button class="aspect-square" severity="danger" outlined>
-      <LogOutIcon class="h-full w-full" />
+      <LogOutIcon @click="userStore.logOut" class="h-full w-full" />
     </Button>
   </div>
 </template>
