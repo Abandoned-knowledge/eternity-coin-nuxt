fe9390606aacfc0753cca0ecaf84e668dfc7b7fd
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-09-06 10:22:12 +0300

    fix: add backend to UpdateCategory

--- src/features/Popup/UpdateCategory/ui/UpdateCategory.vue
+++ src/features/Popup/UpdateCategory/ui/UpdateCategory.vue
@@ -1,12 +1,51 @@
 <script setup lang="ts">
-const newLabel = ref();
 const categoryStore = useCategoryStore();
-const color = computed(() => {
-  return categoryStore.currentCategory ? categoryStore.currentCategory.color : null;
-});
-const label = computed(() => {
+const userStore = useUserStore();
+const supabase = useSupabaseClient();
+const toast = useToast();
+
+function showToast(severity: primeVueSeverity, msg: string) {
+  toast.add({ severity: severity, detail: msg, life: 2000 });
+}
+
+const defaultLabel = computed(() => {
   return categoryStore.currentCategory ? categoryStore.currentCategory.label : "New label";
 });
+
+const newLabel = ref();
+const newColor = ref(categoryStore.currentCategory?.color);
+
+watch(categoryStore, () => {
+  newColor.value = categoryStore.currentCategory?.color;
+});
+
+async function submitForm() {
+  if (userStore.user && categoryStore.currentCategory) {
+    const updatedLabel = newLabel.value ? newLabel.value : defaultLabel.value;
+    const updatedColor = newColor.value?.slice(0, 1) == "#" ? newColor.value : `#${newColor.value}`;
+
+    const { error } = await supabase
+      .from("categories")
+      .update({
+        label: updatedLabel,
+        color: updatedColor,
+      })
+      .eq("id", categoryStore.currentCategory.id);
+
+    if (error) {
+      showToast("error", error.message);
+    } else {
+      showToast("success", `Update name ${defaultLabel.value} on ${newLabel.value}`);
+      await (categoryStore.currentCategory.type == "income"
+        ? categoryStore.fetchIncome()
+        : categoryStore.fetchExpense());
+
+      categoryStore.currentCategory = null;
+      categoryStore.updateCategoryIsVisible = false;
+      newLabel.value = null;
+    }
+  }
+}
 </script>
 
 <template>
@@ -16,18 +55,18 @@ const label = computed(() => {
     modal
     header="Update category"
   >
-    <form @submit.prevent class="form">
+    <form @submit.prevent="submitForm" class="form">
       <FloatLabel class="w-full">
-        <InputText placeholder="New label" class="w-full" id="new_label" v-model="newLabel" />
-        <label for="new_label">{{ label }}</label>
+        <InputText id="label" v-model="newLabel" />
+        <label for="label">{{ defaultLabel }}</label>
       </FloatLabel>
 
       <div class="flex items-center gap-2">
-        <p class="text-gray">select the new color</p>
-        <ColorPicker v-model="color" format="hex" />
+        <p class="text-gray">select the color</p>
+        <ColorPicker @value="console.log(123)" v-model="newColor" format="hex" />
       </div>
 
-      <Button label="submit" severity="contrast" />
+      <Button label="submit" type="submit" severity="contrast" />
     </form>
   </Dialog>
 </template>
