f77081bda1439a4d822aee2bc23cf4e34cedf664
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-09-09 18:25:59 +0300

    create [GET] api/transaction/:type

---
+++ src/server/api/transactions/[type].get.ts
@@ -0,0 +1,16 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+  const client = await serverSupabaseClient(event);
+  const { type } = getRouterParams(event);
+  const { user_id } = getQuery(event);
+
+  const response = await client
+    .from("transactions")
+    .select(`id, date, value, categories(id, type, label, color)`)
+    .eq("categories.type", type)
+    .eq("categories.user_id", user_id)
+    .not("categories", "is", null);
+
+  return response;
+});
--- src/shared/store/transaction.ts
+++ src/shared/store/transaction.ts
@@ -1,4 +1,6 @@
 export const useTransactionStore = defineStore("transaction", () => {
+  const user = useSupabaseUser();
+
   const donutChartDataIncome = ref<donutData[] | null>(null);
   const donutChartDataExpense = ref<donutData[] | null>(null);
   const lineChartDataIncome = ref<lineData[] | null>(null);
@@ -6,6 +8,28 @@ export const useTransactionStore = defineStore("transaction", () => {
   const incomeData = ref<ITransactionData[] | null>(null);
   const expenseData = ref<ITransactionData[] | null>(null);
   const allData = ref<ITransactionData[] | null>(null);
+
+  async function fetchIncome() {
+    const { data, error } = await $fetch("/api/transactions/income", {
+      method: "get",
+      params: {
+        user_id: user.value?.id,
+      },
+    });
+
+    error ? (incomeData.value = null) : (incomeData.value = <ITransactionData[]>data);
+  }
+
+  async function fetchExpense() {
+    const { data, error } = await $fetch("/api/transactions/expense", {
+      method: "get",
+      params: {
+        user_id: user.value?.id,
+      },
+    });
+
+    error ? (expenseData.value = null) : (expenseData.value = <ITransactionData[]>data);
+  }
 
   return {
     donutChartDataExpense,
@@ -15,5 +39,7 @@ export const useTransactionStore = defineStore("transaction", () => {
     incomeData,
     expenseData,
     allData,
+    fetchIncome,
+    fetchExpense,
   };
 });
