fb503782f1501c2ecdbfe094081c7a711c5cc314
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-09-11 14:51:22 +0300

    fix: add border-b-transparent to Header -> Navigation

--- src/widgets/header/ui/Navigation.vue
+++ src/widgets/header/ui/Navigation.vue
@@ -5,7 +5,7 @@ import { menuItems } from "../";
 <template>
   <nav class="flex items-center gap-5">
     <NuxtLink
-      class="flex items-center gap-2 px-2 pb-1 font-light text-gray transition-all duration-100"
+      class="flex items-center gap-2 px-2 pb-1 font-light text-gray transition-all duration-100 border-b border-transparent"
       v-for="item in menuItems"
       :to="item.route"
     >
@@ -17,7 +17,7 @@ import { menuItems } from "../";
 
 <style lang="scss" scoped>
 .router-link-exact-active {
-  @apply border-b border-b-dark;
+  @apply border-b-dark;
 
   & * {
     @apply font-normal text-dark;
